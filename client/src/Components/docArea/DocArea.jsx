f60a7de85989de5c946aca583ff11c6cca862103
Sachita Nand <shachitanandk@gmail.com>
2023-08-08 02:27:53 +0530

    ..

---
+++ client/src/Components/docArea/DocArea.jsx
@@ -0,0 +1,19 @@
+
+import Code from '../code/code'
+import Response from '../code/response'
+
+const DocArea=()=> {
+  return (
+    <div>
+        <div className="text-[3rem] font-bold font-Roboto mt-5"><span>Product</span></div>
+        <div className="text-[2rem] font-bold font-Roboto mt-5"><span>Get All Product</span></div>
+        <div className="text-[1rem] font-normal font-Roboto mt-3 mb-4"><span>You can access the list of 200 products by using the <span className='bg-black text-white'>/products</span> endpoint.</span></div>
+        <span className='text-[1.2rem] font-semibold font-Roboto mb-4'>Request:</span>
+        <Code />
+        <span className='text-[1.2rem] font-semibold font-Roboto mb-4'>Response:</span>
+        <Response />
+    </div>
+  )
+}
+
+export default DocArea
